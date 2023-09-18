14a32b232d7ce49017858845f6143dd9ba9e8890
Lovedeep <gurugobind.sh@gmail.com>
2023-09-18 05:34:28 +0530

    sidebar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,24 +1,22 @@
-"use client"
-import React from "react";
-import {
-  IoIosBriefcase,
-  IoIosNotifications,
-} from "react-icons/io";
+"use client";
+import React, { useState } from "react";
+import { IoIosBriefcase, IoIosNotifications } from "react-icons/io";
 import { LiaSearchSolid } from "react-icons/lia";
 import { BsPersonVideo3 } from "react-icons/bs";
 import { RiArrowDownSFill, RiCheckboxMultipleFill } from "react-icons/ri";
 import { useMediaQuery } from "react-responsive";
 
 type NavbarProps = {
-  isOpen: boolean,
+  isOpen: boolean;
 };
 
-const Navbar: React.FC<NavbarProps> = ({isOpen}) => {
+const Navbar: React.FC<NavbarProps> = ({ isOpen }) => {
   let isTabletMid = useMediaQuery({ query: "(max-width: 768px)" });
+  const [modal, setModal] = useState(false);
 
   return (
     <>
-      <header className={`bg-theme flex-wrap ${!isOpen? "ml-10": ""}`}>
+      <header className={`bg-theme flex-wrap ${!isOpen ? "ml-10" : ""}`}>
         <div className=" flex flex-wrap items-center justify-between mx-auto xs:p-2 px-4 py-2">
           <form className={`flex-grow max-w-sm`}>
             <label
@@ -41,7 +39,9 @@ const Navbar: React.FC<NavbarProps> = ({isOpen}) => {
               />
             </div>
           </form>
-          <div className={`ml-2 mr-2 hidden md:flex lg:flex hover:cursor-pointer`}>
+          <div
+            className={`ml-2 mr-2 hidden md:flex lg:flex hover:cursor-pointer`}
+          >
             <p className="underline text-sm leading-5 text-right font-semibold text-selected">
               OTHER MENUS
             </p>
@@ -95,56 +95,49 @@ const Navbar: React.FC<NavbarProps> = ({isOpen}) => {
                 </li>
               </ul>
             </div>
-            <div className="bg-components items-center hidden mr-4 lg:flex justify-between w-32 p-2 rounded-full hover:cursor-pointer" onClick={() => {}}>
+            <div
+              className="bg-components items-center hidden mr-4 lg:flex justify-between w-32 p-2 rounded-full hover:cursor-pointer"
+              onClick={() => {}}
+            >
               <div className="w-5 h-5 bg-blue rounded-full pr-1"></div>
               <text className="text-[#ffff]">English</text>
               <div className="pl-1 text-selected">
                 <RiArrowDownSFill />
               </div>
             </div>
-            <div className="bg-[#a3a0b0] rounded-xl h-8 w-8 ml-2">
-            </div>
-            <div className={`m-2 flex-col ${isTabletMid? 'hidden': ''}`} id="user_details">
-              <p className="font-normal	text-sm leading-5 text-right text-[#ffffff]">
-                Instructor Day
-              </p>
-              <p className="font-normal text-[10px] leading-4 text-right text-[#787486]">
-                Super Admin
-              </p>
-            </div>
-            <button
-              type="button"
-              className="flex mr-3 text-sm rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300"
-              id="user-menu-button"
-              aria-expanded="false"
-              data-dropdown-toggle="user-dropdown"
-              data-dropdown-placement="bottom"
-            >
-              <span className="sr-only">Open user menu</span>
-            </button>
-            <div className="m-2 text-selected">
-              <RiArrowDownSFill />
-            </div>
-            {/* <div
-              className="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow :bg-gray-700 :divide-gray-600"
-              id="user-dropdown"
-            > */}
-            {/* <div className="px-4 py-3">
-                <span className="block text-sm text-gray-900 :text-white">
-                  Bonnie Green
-                </span>
+            <div className="hover:cursor-pointer flex items-center" onClick={() => setModal(!modal)}>
+              <div className="bg-[#a3a0b0] rounded-xl h-8 w-8 ml-2"></div>
+              <div
+                className={`m-2 flex-col ${isTabletMid ? "hidden" : ""}`}
+                id="user_details"
+              >
+                <p className="font-normal	text-sm leading-5 text-right text-[#ffffff]">
+                  Instructor Day
+                </p>
+                <p className="font-normal text-[10px] leading-4 text-right text-[#787486]">
+                  Super Admin
+                </p>
               </div>
-              <ul className="py-2" aria-labelledby="user-menu-button">
-                <li>
-                  <a
-                    href="#"
-                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 :hover:bg-gray-600 :text-gray-200 :hover:text-white"
-                  >
-                    Dashboard
-                  </a>
-                </li>
-              </ul>
-            </div> */}
+              <div className="m-2 text-selected">
+                <RiArrowDownSFill />
+              </div>
+              {modal && (
+              <div
+                className="z-50 absolute top-[60px] w-[160px] right-7 text-base text-[#ffff] bg-components rounded-lg shadow"
+              >
+                <ul className="py-2" aria-labelledby="user-menu-button">
+                  <li>
+                    <a
+                      href="#"
+                      className="block px-4 py-2 text-sm hover:bg-gray-100 :text-gray-200"
+                    >
+                      Dashboard
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            )}
+            </div>
           </div>
         </div>
       </header>
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,7 +1,10 @@
 "use client"
 import React, { Dispatch, SetStateAction } from 'react';
 import { RxHamburgerMenu } from 'react-icons/rx';
-import { MdSpeed } from 'react-icons/md';
+import { MdEmail, MdOutlineKeyboardArrowRight, MdSpeed } from 'react-icons/md';
+import { IoIosArrowDown, IoIosArrowDropright } from 'react-icons/io';
+import { IoChatbubblesSharp } from 'react-icons/io5';
+import { HiMiniSquares2X2 } from 'react-icons/hi2';
 
 type SidebarProps = {open: boolean, setOpen: Dispatch<SetStateAction<boolean>>};
 
@@ -19,12 +22,47 @@ const Sidebar:React.FC<SidebarProps> = ({open, setOpen}) => {
                 </button>
             </div>
             <div className='pt-10'>
-                <text className='text-[#ffffffc1] flex pl-8'>MAIN MENU</text>
+                <text className={`text-[#ffffffc1] flex pl-8 ${open? "hidden": ""}`}>MAIN MENU</text>
             </div>
             <div className='pt-8'>
-                <div className='text-[#ffffffc1] flex items-start justify-around '>
-                    <MdSpeed className='text-lg' />
-                    <text>Dashboard</text>
+                <div className='text-[#464366] hover:cursor-pointer flex items-center justify-around '>
+                    <div className='flex items-center'>
+                    <MdSpeed className='text-3xl' />
+                    <text className={`${open? "hidden": ""} pl-4`} >dashboard</text>
+                    </div>
+                    <div className='w-0'></div>
+                </div>
+            </div>
+            <div className='pt-8'>
+                <div className='text-[#464366] hover:cursor-pointer flex items-center justify-around '>
+                    <div className='flex items-center'>
+                    <MdEmail className='text-3xl' />
+                    <text className={`${open? "hidden": ""} pl-4`} >Email</text>
+                    </div>
+                    <div className={`${open? "hidden": ""} flex`}>
+                    <div className="w-5 h-5 bg-blue rounded-full text-[#ffff] text-[10px] flex items-center justify-center">17</div>
+                    <IoIosArrowDown className={`pl-1 text-items`} />
+                    </div>
+                </div>
+            </div>
+            <div className='pt-8'>
+                <div className='text-items hover:cursor-pointer flex items-center justify-around '>
+                    <div className='flex items-center'>
+                    <IoChatbubblesSharp className='text-3xl' />
+                    <text className={`${open? "hidden": ""} pl-4 mr-7`} >Chat</text>
+                    </div>
+                    <div className={`${open? "hidden": ""} w-2`}></div>
+
+                </div>
+            </div>
+            <div className='pt-8'>
+                <div className='text-selected hover:cursor-pointer flex items-center justify-around '>
+                    <div className='flex items-center'>
+                    <HiMiniSquares2X2 className='text-3xl' />
+                    <text className={`${open? "hidden": ""} pl-4 `} >Kanban</text>
+                    </div>
+                    <MdOutlineKeyboardArrowRight className={`pl-1 text-items`} />
+
                 </div>
             </div>
         </div>
